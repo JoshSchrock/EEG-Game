c9dde5636e5f2f7938b0bceb60dccaf1bf2423d9
Josh <joshuaqschrock@hotmail.com>
2022-06-17 14:52:28 -0400

    Done

--- MNE.py
+++ MNE.py
@@ -1,13 +1,47 @@
 import os
 import numpy as np
 import mne
+import matplotlib.pyplot as plt
 
-data_raw_file = r"D:\Pycharm Projects\EEG-Game\BCI Evasion 2\EEGExports\EEG-Game_Josh Schrock_EPOCFLEX-F0000172_EPOCFLEX_123045_2022.06.15T16.01.52.04.00.edf"
-raw = mne.io.read_raw_edf(data_raw_file)
+# good channels: [4,5,6,7,8,9,10,11,12,13,14,15,16,17]
+# all channels ['TIME_STAMP_s', 'TIME_STAMP_ms', 'COUNTER', 'INTERPOLATED', 'AF3', 'F7', 'F3', 'FC5', 'T7', 'P7', 'O1', 'O2', 'P8', 'T8', 'FC6', 'F4', 'F8', 'AF4', 'HighBitFlex', 'SaturationFlag', 'RAW_CQ', 'BATTERY', 'BATTERY_PERCENT', 'MarkerIndex', 'MarkerType', 'MarkerValueInt', 'MARKER_HARDWARE', 'CQ_AF3', 'CQ_F7', 'CQ_F3', 'CQ_FC5', 'CQ_T7', 'CQ_P7', 'CQ_O1', 'CQ_O2', 'CQ_P8', 'CQ_T8', 'CQ_FC6', 'CQ_F4', 'CQ_F8', 'CQ_AF4', 'CQ_OVERALL', 'EQ_SampleRateQua', 'EQ_OVERALL', 'EQ_AF3', 'EQ_F7', 'EQ_F3', 'EQ_FC5', 'EQ_T7', 'EQ_P7', 'EQ_O1', 'EQ_O2', 'EQ_P8', 'EQ_T8', 'EQ_FC6', 'EQ_F4', 'EQ_F8', 'EQ_AF4', 'CQ_CMS', 'CQ_DRL']
+stim_channels = ['MarkerIndex', 'MarkerType', 'MarkerValueInt', 'MARKER_HARDWARE']
+exclude = ['TIME_STAMP_s', 'TIME_STAMP_ms', 'COUNTER', 'INTERPOLATED','HighBitFlex', 'SaturationFlag', 'RAW_CQ', 'BATTERY', 'BATTERY_PERCENT', 'CQ_AF3', 'CQ_F7', 'CQ_F3', 'CQ_FC5', 'CQ_T7', 'CQ_P7', 'CQ_O1', 'CQ_O2', 'CQ_P8', 'CQ_T8', 'CQ_FC6', 'CQ_F4', 'CQ_F8', 'CQ_AF4', 'CQ_OVERALL', 'EQ_SampleRateQua', 'EQ_OVERALL', 'EQ_AF3', 'EQ_F7', 'EQ_F3', 'EQ_FC5', 'EQ_T7', 'EQ_P7', 'EQ_O1', 'EQ_O2', 'EQ_P8', 'EQ_T8', 'EQ_FC6', 'EQ_F4', 'EQ_F8', 'EQ_AF4', 'CQ_CMS', 'CQ_DRL']
+
+#  create raw object with only eeg and markers, exculde all other info
+edf_file_name = 'EEG-Game_Josh Schrock_EPOCFLEX-F0000172_EPOCFLEX_123045_2022.06.15T16.01.52.04.00'
+data_raw_file = f"{os.getcwd()}\\BCI Evasion 2\\EEGExports\\{edf_file_name}.edf"
+raw = mne.io.read_raw_edf(data_raw_file, stim_channel=stim_channels, exclude=exclude)
+
+# Form the 10-20 montage
+mont1020 = mne.channels.make_standard_montage('standard_1020')
+# Choose what channels you want to keep
+# Make sure that these channels exist e.g. T1 does not exist in the standard 10-20 EEG system!
+kept_channels = ['AF3', 'F7', 'F3', 'FC5', 'T7', 'P7', 'O1', 'O2', 'P8', 'T8', 'FC6', 'F4', 'F8', 'AF4']
+ind = [i for (i, channel) in enumerate(mont1020.ch_names) if channel in kept_channels]
+mont1020_new = mont1020.copy()
+# Keep only the desired channels
+mont1020_new.ch_names = [mont1020.ch_names[x] for x in ind]
+kept_channel_info = [mont1020.dig[x+3] for x in ind]
+# Keep the first three rows as they are the fiducial points information
+mont1020_new.dig = mont1020.dig[0:3]+kept_channel_info
+mont1020_new.plot()
+raw = raw.set_montage(mont1020_new)
+
 
 raw.plot_psd(fmax=50)
-raw.plot(duration=5, n_channels=30)
+raw.plot(duration=30, n_channels=30)
 
-mag_channels = mne.pick_types(raw.info, meg='mag')
-raw.plot(duration=60, order=mag_channels, n_channels=len(mag_channels),
-         remove_dc=False)
+events = mne.find_events(raw)
+
+# try:
+#     mne.set_config('MNE_USE_CUDA', True)
+# except TypeError as err:
+#     print(err)
+
+# ig = plt.figure()
+# ax2d = fig.add_subplot(121)
+# ax3d = fig.add_subplot(122, projection='3d')
+# raw.plot_sensors(ch_type='eeg', axes=ax2d)
+# raw.plot_sensors(ch_type='eeg', axes=ax3d, kind='3d')
+# ax3d.view_init(azim=70, elev=15)
